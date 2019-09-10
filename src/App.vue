119d3d3a0a95d407607ae2553b2b398d96a2a68c
iamcoderloser <642872728@qq.com>
2019-09-10 21:21:46 +0800

    vuex的使用

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>{{count}}</h1>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import { mapState } from "vuex";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["count"])
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import Vuex from "vuex";
+
+Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        decrement(state) {
+            state.count--;
+        }
+    },
+    actions: {},
+    getters: {}
+});
+/* eslint-disable no-new */
+new Vue({
+    el: "#app",
+    components: {
+        App
+    },
+    template: "<App/>",
+    store
+});
